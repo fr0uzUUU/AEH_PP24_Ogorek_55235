7c54a4ff3a61aaae0d9867a2cfce8c2191a82b89
Michal <michal.ogorek0@gmail.com>
2024-02-27 10:21:54 +0100

    Pierwszy zapis do repozytorium

--- Lab1/src/pl/pp/mojaPierwszaAplikacja.java
+++ Lab1/src/pl/pp/mojaPierwszaAplikacja.java
@@ -2,6 +2,6 @@ package pl.pp;
 
 public class mojaPierwszaAplikacja {
     public static void main(String[] args){
-        System.out.println("WITAJ PRM2T 55235");
+        System.out.println("WITAJ PRM2T 55235 ");
     }
 }
