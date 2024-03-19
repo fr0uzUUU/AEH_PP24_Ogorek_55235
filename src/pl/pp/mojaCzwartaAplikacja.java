443c0dd0ec04b1655db4a7a51985a1371330c126
Michal <michal.ogorek0@gmail.com>
2024-03-19 10:20:40 +0100

    Lab 4

--- src/pl/pp/mojaCzwartaAplikacja.java
+++ src/pl/pp/mojaCzwartaAplikacja.java
@@ -3,48 +3,81 @@ import java.util.Scanner;
 public class mojaCzwartaAplikacja {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj proszę liczbę większą od 100:");
-        //pętla WHILE (https://introcs.cs.princeton.edu/java/11cheatsheet/images/while.png)
-        var number1 = scanner.nextDouble();
-        while (number1 <= 100) {
-            System.out.println("Liczba nie jest większa od 100, podaj jeszcze raz:");
-            number1 = scanner.nextDouble();
-        }
-        System.out.println("Dziękuję! Podałeś liczbę: " + number1);
-        //pętla DO..WHILE (https://introcs.cs.princeton.edu/java/11cheatsheet/images/do-while.png)
-        double number2;
-        do {
-            System.out.println("Podaj proszę liczbę większą od 200:");
-            number2 = scanner.nextDouble();
-        } while (number2 <= 200);
-        System.out.println("Dziękuję! Podałeś liczbę: " + number2);
-        //pętla FOR (https://introcs.cs.princeton.edu/java/11cheatsheet/images/for.png)
-        int wynik = 0;
-        for (var i = 1; i <= 10; i++) {
-            wynik = wynik + i;
-            System.out.println("Przebieg numer " + i + " w pętli for, a zmienna wynik = " + wynik);
-        }
-        //instrukcje warunkowe IF..ELSE (https://introcs.cs.princeton.edu/java/11cheatsheet/images/if.png)
-        System.out.println("Podaj proszę liczbę x: ");
-        var x = scanner.nextDouble();
-        System.out.println("Podaj proszę liczbę y: ");
-        var y = scanner.nextDouble();
-        if (x > y) {
-            System.out.println("x jest większe od y");
-        } else if (x < y) {
-            System.out.println("x jest mniejsze od y");
-        } else {
-            System.out.println("x jest równe y");
-        }
-        //zakończenie programu za pomocą wpisania odpowiedniego klawisza
-        while (true) {
-            System.out.println("Wpisz wartość -1 żeby wyjść z programu");
-            var input = scanner.nextDouble();
-            if (input == -1) {
-                System.out.println("Wyjście...");
+
+        /*while (true) {
+            System.out.println("Enter lower and upper integer limits: ");
+            int input1 = scanner.nextInt();
+            int input2 = scanner.nextInt();
+            if (input1 >= input2) {
+                System.out.println("Done...");
                 break;
             }
+            int sum = 0;
+            for (int i = input1; i <= input2; i++){
+                sum += i*i;
+            }
+            System.out.println("The sums of the squares from " + input1 + " to " + input2 + " is " + sum);
+        }*/
+        System.out.println("Calculator");
+        //https://www.w3schools.com/java/java_try_catch.asp
+        //https://www.w3schools.com/java/java_switch.asp
+
+        while (true) {
+
+            System.out.print("Enter numbers: (type 'q' to quit)");
+            String input = scanner.next();
+
+            if (input.equalsIgnoreCase("q")) {
+                System.out.println("Done!");
+                break;
+            }
+
+            double num1;
+            try {
+                num1 = Double.parseDouble(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input!");
+                continue;
+            }
+
+            double num2;
+            try {
+                num2 = scanner.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+                scanner.next();
+                continue;
+            }
+
+            System.out.print("Choose operation (+, -, *, /): ");
+            char operation = scanner.next().charAt(0);
+
+            double result;
+
+            switch (operation) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("Division by zero is not allowed.");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Error!");
+                    continue;
+            }
+            System.out.println("Result: " + result);
         }
+
         scanner.close();
     }
 }
